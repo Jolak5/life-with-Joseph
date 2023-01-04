eed5d02fa835b16f3d49c4abcaba82c02f7e9857
jolak5 <kayodej4@gmail.com>
2023-01-04 15:20:02 +0100

    create mobile and desktop version

---
+++ global.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-plusplus */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector("body");
+const navItem = document.querySelectorAll(".nav-item");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.style.overflow = "hidden";
+});
+for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    hamburger.classList.toggle("active");
+    body.style.overflow = "visible";
+  });
+}
